893e0f6018fe43a74c1a45fa57d1ebccd967b083
Tom <twang10@gmail.com>
2015-04-22 16:03:30 -0400

    weird if statements

--- wiki_proj/scripts/to_GeoJSON.py
+++ wiki_proj/scripts/to_GeoJSON.py
@@ -6,8 +6,11 @@ def main():
     new_data = {}
     new_data["type"] = "FeatureCollection"
     new_data["features"] = []
+    america = new_data
+    south_america = new_data
     with open("../json/settlements_locs.json","r") as f:
         raw_data = json.load(f)
+        stop = 0
         for entry in raw_data:
         	feature = {"type":"", 
         			   "geometry":{"coordinates":[]},
@@ -21,9 +24,16 @@ def main():
         	feature["properties"]["type"] = entry["_TYPE"]
         	feature["properties"]["est_date"] = entry["established_date"]
         	feature["properties"]["ext_date"] = entry["extinct_date"]
-        	new_data["features"].append(feature)
-    with open ("../json/settlements_geo.json", "w") as f:
-    	json.dump(new_data, f)
+        	# if entry["lat"] >= 24 and entry["lat"] <= 50 and -125 <= entry["lng"] and entry["lng"] <= -66:
+	        # 	america["features"].append(feature)
+        	if entry["lat"] >= -57 and entry["lat"] <= 14 and -94 <=entry["lng"] and entry["lng"] <= -32:
+        		south_america["features"].append(feature)
+        	if(entry["lat"] >= 0) and (entry["lat"] <= 0):
+        		america["features"].append(feature)
+    with open ("../json/america.json", "w") as f:
+    	json.dump(america, f)
+    with open ("../json/south_america.json", "w") as f:
+    	json.dump(south_america, f)
 
     # Example object
     # { "type": "Feature",
