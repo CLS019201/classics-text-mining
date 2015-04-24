1cf3fede100f25041f759bb4a85c81cf84bb6ded
Tom <twang10@gmail.com>
2015-04-24 08:44:10 -0400

    fixed JSON bug
        america = new_data
        south_america = new_data
        america = copy.deepcopy(new_data)
        south_america = copy.deepcopy(new_data)
    !!!!!!

--- wiki_proj/scripts/to_GeoJSON.py
+++ wiki_proj/scripts/to_GeoJSON.py
@@ -1,13 +1,13 @@
 # Converts our current JSON to GeoJSON in preparation for D3 map viz
 
-import json
+import json, copy
 
 def main():
     new_data = {}
     new_data["type"] = "FeatureCollection"
     new_data["features"] = []
-    america = new_data
-    south_america = new_data
+    america = copy.deepcopy(new_data)
+    south_america = copy.deepcopy(new_data)
     with open("../json/settlements_locs.json","r") as f:
         raw_data = json.load(f)
         stop = 0
@@ -24,12 +24,10 @@ def main():
         	feature["properties"]["type"] = entry["_TYPE"]
         	feature["properties"]["est_date"] = entry["established_date"]
         	feature["properties"]["ext_date"] = entry["extinct_date"]
-        	# if entry["lat"] >= 24 and entry["lat"] <= 50 and -125 <= entry["lng"] and entry["lng"] <= -66:
-	        # 	america["features"].append(feature)
-        	if entry["lat"] >= -57 and entry["lat"] <= 14 and -94 <=entry["lng"] and entry["lng"] <= -32:
+        	if entry["lat"] >= 24 and entry["lat"] <= 50 and -125 <= entry["lng"] and entry["lng"] <= -66:
+	        	america["features"].append(feature)
+        	if entry["lat"] >= -57.0 and entry["lat"] <= 14.0 and -94.0 <=entry["lng"] and entry["lng"] <= -32.0:
         		south_america["features"].append(feature)
-        	if(entry["lat"] >= 0) and (entry["lat"] <= 0):
-        		america["features"].append(feature)
     with open ("../json/america.json", "w") as f:
     	json.dump(america, f)
     with open ("../json/south_america.json", "w") as f:
